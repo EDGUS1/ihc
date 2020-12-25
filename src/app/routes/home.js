8127a4d2950f6ee17fd67ae391b37d030cd8b547
EDGUS1 <eduardo.navarro2@unmsm.edu.pe>
2020-12-24 23:26:33 -0500

    v0.0.6

--- src/app/routes/home.js
+++ src/app/routes/home.js
@@ -24,6 +24,26 @@ module.exports = app => {
         });
     });
 
+    app.get('/login',(req, res) => {
+        
+        connection.query('SELECT * FROM ADMINISTRADOR', (err, result) => {
+            /* console.log('Datos ', result); */
+            res.render('login/login',{
+                resultado: result
+            });
+        });
+    });
+
+    app.get('/seleccionado',(req, res) => {
+        
+        connection.query('SELECT * FROM ADMINISTRADOR', (err, result) => {
+            /* console.log('Datos ', result); */
+            res.render('seleccionado/seleccionado',{
+                resultado: result
+            });
+        }); 
+    });
+
 
     app.post('/home', (req, res) => {
         const {id, title, news, pass} = req.body;
@@ -36,5 +56,19 @@ module.exports = app => {
         } , (err, result) =>{
             res.redirect('/');
         });
+    });
+
+    app.post('/verificar', (req, res) => {
+        /* const {id, title, news, pass} = req.body; */
+        
+        /* connection.query('INSERT INTO usuario SET?',{
+            id_usuario: id,
+            nombre_completo_usuario: title,
+            correo: news,
+            contrasenia: pass
+        } , (err, result) =>{
+            res.redirect('/seleccionado');
+        }); */
+        res.redirect('/seleccionado');
     });
 } 
