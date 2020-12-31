bd78005aa8e724d42e271680f33768cc356fac94
EDGUS1 <eduardo.navarro2@unmsm.edu.pe>
2020-12-31 11:36:52 -0500

    v0.3.4

--- src/app/public/index.js
+++ src/app/public/index.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-    $('#data').DataTable( {
+    $('#data').DataTable({
         language: {
             url: "https://cdn.datatables.net/plug-ins/1.10.22/i18n/Spanish.json"
         },
-        "lengthChange":false,
+        "lengthChange": false,
         "order": [],
         "pageLength": 5
-    } );
-} );
+    });
+});
 /* sticky menu */
 
 window.addEventListener("scroll", () => {
     let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0); 
+    header.classList.toggle("sticky", window.scrollY > 0);
 })
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFyY29zdiIsImEiOiJja2liNHF2bWkwNjlyMnFsYWVtbXo3cGI5In0.oTCXFk1EXHOjTjMSdu6ZzQ';
@@ -20,7 +20,6 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFyY29zdiIsImEiOiJja2liNHF2bWkwNjlyMnFsYWVtb
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10',
-    center: [-77.02824,-12.04318],
+    center: [-77.02824, -12.04318],
     zoom: 4
-
 });
