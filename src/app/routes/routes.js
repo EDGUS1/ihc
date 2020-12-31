d9055e5045d190ff233a7ebc2c4df2870e7ceac5
EDGUS1 <eduardo.navarro2@unmsm.edu.pe>
2020-12-30 20:33:48 -0500

    Convertir las rutas

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -5,88 +5,98 @@ const router = express.Router();
 const pool = require('../../config/database');
 
 router.get('/', async(req, res) => {
-    /* const links = await pool.query('SELECT * FROM HOSPITAL'); */
-    res.render('home')
+    const links = await pool.query('SELECT * FROM HOSPITAL');
+    res.render('home', { resultado: links })
 });
 
 module.exports = router;
-//npm run dev
-/* module.exports = app => {
 
-    const connection = dbConnection();
+router.get('/verificar', (req, res) => {
+    res.render('verificar');
+})
 
-    app.get('/', (req, res) => {
 
-        connection.query('SELECT * FROM HOSPITAL', (err, result) => {
-            res.render('home', {
-                resultado: result
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+
+router.get('', (req, res) => {
+
+})
+
+router.get('', (req, res) => {
+
+})
+
+router.get('', (req, res) => {
+
+})
+router.get('', (req, res) => {
+
+})
+
+router.get('', (req, res) => {
+
+    })
+    //npm run dev
+    /* module.exports = app => {
+
+        const connection = dbConnection();
+        app.get('/seleccionado', (req, res) => {
+
+            connection.query('SELECT * FROM HOSPITAL', (err, result) => {
+
+                res.render('seleccionado', {
+                    resultado: result
+                });
             });
         });
-    });
-
-    app.get('/verificar', (req, res) => {
-        res.render('verificar');
-    });
-
-    app.get('/login', (req, res) => {
-        res.render('login');
-    });
-
-    app.get('/seleccionado', (req, res) => {
-
-        connection.query('SELECT * FROM HOSPITAL', (err, result) => {
-
-            res.render('seleccionado', {
-                resultado: result
-            });
+        app.get('/noseleccionado', (req, res) => {
+            res.render('noseleccionado');
         });
-    });
-    app.get('/noseleccionado', (req, res) => {
-        res.render('noseleccionado');
-    });
-    app.get('/dashboard', (req, res) => {
-        res.render('dashboard');
-    });
-    app.get('/admin', (req, res) => {
-        res.render('admin');
-    });
+        app.get('/dashboard', (req, res) => {
+            res.render('dashboard');
+        });
+        app.get('/admin', (req, res) => {
+            res.render('admin');
+        });
 
-    app.post('/verificar', (req, res) => {
-        const { dni, nombre } = req.body;
+        app.post('/verificar', (req, res) => {
+            const { dni, nombre } = req.body;
 
-        if (dni == '1' && nombre == 'persona') {
+            if (dni == '1' && nombre == 'persona') {
 
-            res.redirect('seleccionado');
-        } else {
+                res.redirect('seleccionado');
+            } else {
 
-            res.redirect('noseleccionado');
-        }
-    });
-
-    app.post('/login', (req, res) => {
-        const { username, password } = req.body;
-
-        connection.query('SELECT * FROM ADMINISTRADOR', (err, result) => {
-            for (let i = 0; i < result.length; i++) {
-
-                if (username == result[i].nombre_admin && password == result[i].password) {
-
-                    if (result[i].permiso == 'comun') {
-                        res.redirect('admin');
-                        break;
-                    } else if (result[i].permiso == 'superadmin') {
-                        res.redirect('dashboard');
-                    }
+                res.redirect('noseleccionado');
             }
+        });
+
+        app.post('/login', (req, res) => {
+            const { username, password } = req.body;
+
+            connection.query('SELECT * FROM ADMINISTRADOR', (err, result) => {
+                for (let i = 0; i < result.length; i++) {
+
+                    if (username == result[i].nombre_admin && password == result[i].password) {
+
+                        if (result[i].permiso == 'comun') {
+                            res.redirect('admin');
+                            break;
+                        } else if (result[i].permiso == 'superadmin') {
+                            res.redirect('dashboard');
+                        }
+                }
+
+            });
 
         });
 
-    });
+        app.use(express.static(path.join(__dirname, '../public')));
 
-    app.use(express.static(path.join(__dirname, '../public')));
+        app.use(function(req, res, next) {
+            res.status(404).render('404');
+        });
 
-    app.use(function(req, res, next) {
-        res.status(404).render('404');
-    });
-
-} */
+    } */
--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-const app = require('./config/server');
+/* const app = require('./config/server'); */
 
-require('./app/routes/routes')(app);
+/* require('./app/routes/routes')(app); */
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { database } = require('./config/keys');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './app/views'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+/* app.use(sesion({
+        secret: 'mysqlsession',
+        resave: false,
+        saveUninitialized: false,
+        store: new mysqlStore(database)
+    })) */
+/* app.use(require('../app/routes')) */
+app.use(require('./app/routes/routes'))
+
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 app.listen(app.get('port'), () => {
     console.log('Server on port ', app.get('port'));
